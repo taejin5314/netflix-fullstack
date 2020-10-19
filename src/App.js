484816b92ec9a999e813d6d0186c12b87ad5274b
taejin <taejin5314@gmail.com>
2020-10-19 16:26:10 -0400

    Update Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <div className="app">
-    </div>
+    <JumbotronContainer />
   );
 }
 
