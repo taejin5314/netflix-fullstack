59c25f44aea728eb5ce011172ef6e76a8e2d91a4
taejin <taejin5314@gmail.com>
2020-10-20 19:05:01 -0400

    Header: styling

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -2,7 +2,13 @@ import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 export const Background = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : `..images/misc/home-bg.jpg`)}) top left cover no-repeat;
 
+    @media (max-width: 1000px) {
+        ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `backgrond: none;`}
+    }
 `;
 
 export const Frame = styled.div`
@@ -10,11 +16,31 @@ export const Frame = styled.div`
 `;
 
 export const Container = styled.div`
+    display: flex;
+    margin: 0 56px;
+    height: 64px;
+    padding: 18px 0;
+    justify-content: space-between;
+    align-items: center;
 
+    a {
+        display: flex;
+    }
+
+    @media (max-width: 1000px) {
+        margin: 0 30px;
+    }
 `;
 
 export const Logo = styled.img`
+    height: 32px;
+    width:108px;
+    margin-right: 40px;
 
+    @media (min-width: 1449px) {
+        height: 45px;
+        width: 167px;
+    }
 `;
 
 export const ButtonLink = styled(ReactRouterLink)`
