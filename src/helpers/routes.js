d00c7618e593cca6e43415c4498932dec4c0c5c2
taejin <taejin5314@gmail.com>
2020-10-21 19:33:18 -0400

    Custom hooks

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -23,6 +23,31 @@ export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
                 return null;
             }}
         />
+    );
+}
 
+export function ProtectedRoute({ user, children, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) => {
+                if (user) {
+                    return children;
+                }
+
+                if (!user) {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: 'signin',
+                                state: { from: location },
+                            }}
+                        />
+                    )
+                }
+
+                return null;
+            }}
+        />
     )
 }
