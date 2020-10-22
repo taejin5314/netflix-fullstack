35a3846ef047184eb6ca4942690aac79e651dd77
taejin <taejin5314@gmail.com>
2020-10-21 20:03:13 -0400

    browse.js

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
-    return <p>Hello from the browse!</p>
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
+    const slides = selectionFilter({ series, films });
+
+    return <BrowseContainer slides={slides} />
+    // return <p>hello</p>
 }
